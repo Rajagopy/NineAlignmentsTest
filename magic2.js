6f8484b1948da34cec38c9a6f59e24a75bc483e8
Rajagopy <rajgopalraj909@gmail.com>
2022-04-26 20:15:50 +0530

    commit 3 -fix conditional statement issues

--- magic2.js
+++ magic2.js
@@ -1,26 +1,30 @@
 let st_url = window.location.href;
 
-let r = st_url.split("=")[1].split("%26");
+let r =  st_url.split("=")[1].split("%26");
 console.log(r);
 
 let lawcha;
 let gooevi;
 
-if(r[0]<=-5){
-  lawcha = "Choatic";
+let lawchaRes = Number.parseInt(r[0]);
+let gooeviRes = Number.parseInt(r[1]); 
+
+if(lawchaRes<=-5){
+  lawcha = "Chaotic";
 }
-else if(-5< r[0] < 5){
-  lawcha = "Nuetral";
+else if((-5 < lawchaRes) && (lawchaRes <= 5)){
+  console.log(typeof (lawchaRes));
+  lawcha = "Neutral";
 }
 else{
   lawcha = "Lawful";
 }
 
-if(r[1]<=-5){
+if(gooeviRes<=-5){
   gooevi = "Evil";
 }
-else if(-5< r[1] < 5){
-  gooevi = "Nuetral";
+else if((-5< gooeviRes)&&( gooeviRes < 5)){
+  gooevi = "Neutral";
 }
 else{
   gooevi = "Good";
