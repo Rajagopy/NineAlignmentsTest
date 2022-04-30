7121c8cd877becd37f3d6468438a4a501318a0f3
Rajagopy <rajgopalraj909@gmail.com>
2022-04-30 13:34:35 +0530

    added reveal result button and functionality

--- magic2.js
+++ magic2.js
@@ -1,38 +1,37 @@
 let st_url = window.location.href;
 
-let r =  st_url.split("=")[1].split("%26");
+let r = st_url.split("=")[1].split("%26");
 console.log(r);
 
 let lawcha;
 let gooevi;
 
 let lawchaRes = Number.parseInt(r[0]);
-let gooeviRes = Number.parseInt(r[1]); 
+let gooeviRes = Number.parseInt(r[1]);
 
-if(lawchaRes<=-5){
+if (lawchaRes <= -5) {
   lawcha = "Chaotic";
-}
-else if((-5 < lawchaRes) && (lawchaRes <= 5)){
-  console.log(typeof (lawchaRes));
+} else if (-5 < lawchaRes && lawchaRes <= 5) {
+  console.log(typeof lawchaRes);
   lawcha = "Neutral";
-}
-else{
+} else {
   lawcha = "Lawful";
 }
 
-if(gooeviRes<=-5){
+if (gooeviRes <= -5) {
   gooevi = "Evil";
-}
-else if((-5< gooeviRes)&&( gooeviRes < 5)){
+} else if (-5 < gooeviRes && gooeviRes < 5) {
   gooevi = "Neutral";
-}
-else{
+} else {
   gooevi = "Good";
 }
-
-console.log("Final result: "+ lawcha+ gooevi);
-
-document.querySelector("#finalResult").innerHTML =
-  "<p id=resultText> Final result: " +lawcha+" "+ gooevi + "</p>";
+res = lawcha + gooevi;
+console.log("Final result: " + res + ".");
 
 
+
+console.log(res+" This is color changer");
+function reveal(){
+document.getElementById(res).style.backgroundColor = "palegreen";
+}
+document.querySelector("#testTag").textContent = "Hi";
